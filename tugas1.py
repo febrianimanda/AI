1d83b1c8fbfacb072df811d2f43ba31469c2f81d
febrianimanda <febrian.imanda@gmail.com>
2016-02-27 23:37:22 +0700

    Finishing tugas1

--- tugas1.py
+++ tugas1.py
@@ -1,108 +1,100 @@
-import csv
+# Name : Febrian Imanda Effendy
+# NIM : 1103134334
+# Kelas : IF-37-06
+# Algoritma A* Heuristic Search dengan representasi graph
 
-class Vertex:
-	def __init__(self, node, heuristic = 0):
-		self.id = node
-		self.adjacent = {}
-		self.heuristic = heuristic
+graph = {
+	'Bobbia' : {
+		'Terme' : 3,
+		'Piacenza' : 5,
+		'Cesena' : 15
+	},
+	'Piacenza' : {
+		'Terme' : 3,
+		'Carpi' : 3,
+	},
+	'Terme' : {
+		'Emilia' : 2,
+		'Faenza' : 3
+	},
+	'Carpi' : {
+		'Emilia' : 2,
+		'Ferrara' : 8
+	},
+	'Emilia' : {
+		'Imola' : 2
+	},
+	'Imola' : {
+		'Faenza' : 1,
+		'Forli' : 3
+	},
+	'Forli' : {
+		'Cesena' : 2,
+		'Ravenna' : 3
+	},
+	'Faenza' : {
+		'Forli' : 2,
+		'Cesena' : 6
+	},
+	'Cesena' : {
+		'Rimini' : 5
+	},
+	'Ferrara' : {
+		'Imola' : 3,
+		'Ravenna' : 6
+	},
+	'Rimini' : {
+		'Ravenna' : 1	
+	}
+}
 
-	def __str__(self):
-		# return 'vertex ' + str(self.id) + ' menuju -> ' + str([x.id for x in self.adjacent])
-		return 'vertex ' + str(self.id)
+h = {
+	'Ravenna' : 0,
+	'Rimini' : 0.5,
+	'Ferrara' : 5,
+	'Forli' : 2,
+	'Cesena' : 4.5,
+	'Faenza' : 4,
+	'Imola'	: 5,
+	'Emilia' : 6,
+	'Terme' : 7,
+	'Carpi' : 8,
+	'Piacenza' : 10,
+	'Bobbia' : 10.5
+}
+closedlist = []
+cost = 0
 
-	def __repr__(self):
-		return self.__str__()
+def heuristic_search(graph, start, end, closedlist, cost):
+	closedlist.append(start)
+	print 'Closedlist = ', closedlist
+	print 'Biaya yang dibutuhkan saat ini = ', cost
+	if start == end :
+		print ''
+		print '==================================='
+		print ''
+		print 'Rute yang ditemukan : ', closedlist
+		print 'Biaya yang dibutuhkan : ', cost
+	if not graph.has_key(start) :
+		return 'Node ada yang tidak ditemukan'
+	fmin = 100
+	gmin = 100
+	ntemp = start
+	print ''
+	print 'Open = ', graph[start].keys()
+	for node in graph[start]:
+		if not node in closedlist :
+			gtemp = cost + graph[start][node]
+			ftemp = gtemp + h[node]
+			print 'Node',node,', f(',node,') = ',ftemp
+			if ftemp < fmin : 
+				fmin = ftemp
+				gmin = gtemp
+				ntemp = node
+	cost = gmin
+	heuristic_search(graph, ntemp, end, closedlist, cost)
 
-	def add_neighbor(self, neighbor, weight = 0):
-		self.adjacent[neighbor] = weight
-
-	def get_connections(self):
-		return self.adjacent.keys()
-
-	def get_id(self):
-		return self.id
-
-	def get_heuristic(self):
-		return self.heuristic
-
-	def get_weight(self, neighbor):
-		return self.adjacent[neighbor]
-
-	def is_connected(self):
-		return bool(self.adjacent)
-
-class Graph:
-
-	def __init__(self):
-		self.list_vertex = {}
-		self.sum_vertex = 0
-
-	def __iter__(self):
-		return iter(self.list_vertex)
-
-	#Fungsi untuk menambahkan vertex ke dalam graph
-	def add_vertex(self, node, heuristic):
-		self.sum_vertex = self.sum_vertex + 1
-		new_vertex = Vertex(node, heuristic)
-		self.list_vertex[node] = new_vertex
-		return new_vertex
-
-	#Fungsi untuk mengambil vertex dengan index n di dalam graph
-	def get_vertex(self, n): 
-		if n in self.list_vertex:
-			return self.list_vertex[n]
-		else:
-			return None
-
-	#fungsi untuk menambah arah dari 'src' ke 'dest' dengan biaya 'cost'
-	def add_edge(self, src, dest, cost = 0): 
-		#mengecek apakah source ada di dalam graph. Jika tidak ada maka akan ditambah di graph
-		if src not in self.list_vertex:
-			self.add_vertex(src)
-		#mengecek apakah dest ada di dalam graph. Jika tidak ada maka akan ditambah di graph
-		if dest not in self.list_vertex:
-			self.add_vertex(dest)
-
-		self.list_vertex[src].add_neighbor(self.list_vertex[dest], cost)
-
-	#Fungsi untuk mengambil list dari vertex yang terdapat didalam graph
-	def get_vertices(self):
-		return self.list_vertex.keys()
-
-	def heuristic_search(self, frm, dest):
-		found = False
-		closedlist = []
-		current_cost = 0
-		while not found:
-			current_vertex = self.get_vertex(frm)
-			current_cost += current_vertex.get_weight()
-			f  = current_cost + current_vertex.get_heuristic()
-
-
-
-
-if __name__ == '__main__':
-
-	g = Graph()
-
-	g.add_vertex('a', 0)
-	g.add_vertex('b', 10)
-	g.add_vertex('c', 5)
-	g.add_vertex('d', 7)
-	g.add_vertex('e', 8)
-	g.add_vertex('f', 4)
-
-	g.add_edge('a', 'b', 7)  
-	g.add_edge('a', 'c', 9)
-	g.add_edge('a', 'f', 14)
-	g.add_edge('b', 'c', 10)
-	g.add_edge('b', 'd', 15)
-	g.add_edge('c', 'd', 11)
-	g.add_edge('c', 'f', 2)
-	g.add_edge('d', 'e', 6)
-	g.add_edge('e', 'f', 9)
-
-	# for v in g:
-	print g.get_vertices()
-	vert = g.get_vertex('a')
-	print vert.get_connections()
+start = 'Bobbia'
+finish = 'Ravenna'
+print "Start : ", start
+heuristic_search(graph, start, finish, closedlist, cost)
